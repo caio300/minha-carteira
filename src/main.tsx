f38388b42330cfd0068e42f7c5266e4a96381169
caio300 <caioqs.info@gmail.com>
2023-01-19 16:19:07 -0300

    Criado estilos globais

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import Dashboard from './pages/Dashboard'
+import App from './App'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Dashboard />
+    <App />
   </React.StrictMode>,
 )
