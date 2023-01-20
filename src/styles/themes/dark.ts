7f452466caef21892d0039c36f56c4a23a626428
caio300 <caioqs.info@gmail.com>
2023-01-20 10:54:28 -0300

    Criado os temas de cores

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,17 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#1b1f38',
+    secondary: '#252a48',
+    tertiary: '#313862',
+
+    white: '#fff',
+    black: '#000',
+    gray: '#bfbfbf',
+
+    sucesse: '#4e41f0',
+    info: '#f7931b',
+    warnig: '#e44c4e',
+  },
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,17 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#dcdcdc',
+    secondary: '#ffffff',
+    tertiary: '#f5f5f5',
+
+    white: '#000',
+    black: '#fff',
+    gray: '#bfbfbf',
+
+    sucesse: '#03bb85',
+    info: '#3b5998',
+    warnig: '#ff6961',
+  },
+}
