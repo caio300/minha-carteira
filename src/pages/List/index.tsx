f9e82bf40aeef968b32d88db0f7ee7a614cd21a1
caio300 <caioqs.info@gmail.com>
2023-02-14 14:45:03 -0300

    Criado os filtros

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,167 +1,52 @@
 import React from  'react';
 import ContentHeader from '../../components/ContentHeader';
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 import SelectInput from '../../components/SelectInput/index';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
 const List: React.FC = () => {
 
-  const options = [
-    {value: 'Rodrigo', label: 'Rodrigo'},
-    {value: 'Maria', label: 'Maria'},
-    {value: 'Ana', label: 'Rodrigo'},
+  const months = [
+    {value: 7, label: 'Julho'},
+    {value: 8, label: 'Agosto'},
+    {value: 9, label: 'Setembro'},
+  ];
+
+  const years = [
+    {value: 2020, label: 2020},
+    {value: 2019, label: 2019},
+    {value: 2018, label: 2018},
   ];
 
   return (
     <Container>
       <ContentHeader title="Saídas" lineColor='#E44C4E'>
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <Filters>
+        <button
+          type='button'
+          className='tag-filter tag-filter-recurrent'
+        >
+          Recorrentes
+        </button>
+        <button
+          type='button'
+          className='tag-filter tag-filter-eventual'
+        >
+          Eventuais
+        </button>
+      </Filters>
+
       <Content>
         <HistoryFinanceCard
-          cardColor='#313862'
           tagColor='#E44C4E'
           title='Conta de Luz'
           subtitle='25/07/2023'
           amount='R$ 130,00'
         />
-
-        <HistoryFinanceCard
-          cardColor='#313862'
-          tagColor='#E44C4E'
-          title='Conta de Luz'
-          subtitle='25/07/2023'
-          amount='R$ 130,00'
-        />
-
-        <HistoryFinanceCard
-          cardColor='#313862'
-          tagColor='#E44C4E'
-          title='Conta de Luz'
-          subtitle='25/07/2023'
-          amount='R$ 130,00'
-        />
-
-        <HistoryFinanceCard
-          cardColor='#313862'
-          tagColor='#E44C4E'
-          title='Conta de Luz'
-          subtitle='25/07/2023'
-          amount='R$ 130,00'
-        />
-
-        <HistoryFinanceCard
-          cardColor='#313862'
-          tagColor='#E44C4E'
-          title='Conta de Luz'
-          subtitle='25/07/2023'
-          amount='R$ 130,00'
-        />
-
-        <HistoryFinanceCard
-          cardColor='#313862'
-          tagColor='#E44C4E'
-          title='Conta de Luz'
-          subtitle='25/07/2023'
-          amount='R$ 130,00'
-        />
-
-        <HistoryFinanceCard
-          cardColor='#313862'
-          tagColor='#E44C4E'
-          title='Conta de Luz'
-          subtitle='25/07/2023'
-          amount='R$ 130,00'
-        />
-
-<HistoryFinanceCard
-          cardColor='#313862'
-          tagColor='#E44C4E'
-          title='Conta de Luz'
-          subtitle='25/07/2023'
-          amount='R$ 130,00'
-        />
-        <HistoryFinanceCard
-          cardColor='#313862'
-          tagColor='#E44C4E'
-          title='Conta de Luz'
-          subtitle='25/07/2023'
-          amount='R$ 130,00'
-        />
-
-        <HistoryFinanceCard
-          cardColor='#313862'
-          tagColor='#E44C4E'
-          title='Conta de Luz'
-          subtitle='25/07/2023'
-          amount='R$ 130,00'
-        />
-
-        <HistoryFinanceCard
-          cardColor='#313862'
-          tagColor='#E44C4E'
-          title='Conta de Luz'
-          subtitle='25/07/2023'
-          amount='R$ 130,00'
-        /><HistoryFinanceCard
-        cardColor='#313862'
-        tagColor='#E44C4E'
-        title='Conta de Luz'
-        subtitle='25/07/2023'
-        amount='R$ 130,00'
-      />
-      <HistoryFinanceCard
-        cardColor='#313862'
-        tagColor='#E44C4E'
-        title='Conta de Luz'
-        subtitle='25/07/2023'
-        amount='R$ 130,00'
-      />
-
-      <HistoryFinanceCard
-        cardColor='#313862'
-        tagColor='#E44C4E'
-        title='Conta de Luz'
-        subtitle='25/07/2023'
-        amount='R$ 130,00'
-      />
-
-      <HistoryFinanceCard
-        cardColor='#313862'
-        tagColor='#E44C4E'
-        title='Conta de Luz'
-        subtitle='25/07/2023'
-        amount='R$ 130,00'
-      /><HistoryFinanceCard
-      cardColor='#313862'
-      tagColor='#E44C4E'
-      title='Conta de Luz'
-      subtitle='25/07/2023'
-      amount='R$ 130,00'
-    />
-    <HistoryFinanceCard
-      cardColor='#313862'
-      tagColor='#E44C4E'
-      title='Conta de Luz'
-      subtitle='25/07/2023'
-      amount='R$ 130,00'
-    />
-
-    <HistoryFinanceCard
-      cardColor='#313862'
-      tagColor='#E44C4E'
-      title='Conta de Luz'
-      subtitle='25/07/2023'
-      amount='R$ 130,00'
-    />
-
-    <HistoryFinanceCard
-      cardColor='#313862'
-      tagColor='#E44C4E'
-      title='Conta de Luz'
-      subtitle='25/07/2023'
-      amount='R$ 130,00'
-    />
       </Content>
     </Container>
   )
