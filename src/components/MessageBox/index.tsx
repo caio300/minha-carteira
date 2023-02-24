159333deda51c1a0b173a2d960738b6b803f45e3
caio300 <caioqs.info@gmail.com>
2023-02-24 18:04:19 -0300

    Criado o cartão de status da carteira

---
+++ src/components/MessageBox/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+
+interface IMessageProps {
+  title: string;
+  description: string;
+  footerText: string;
+  icon: string;
+}
+
+const MessageBox: React.FC<IMessageProps> = ({
+  title,
+  description,
+  footerText,
+  icon,
+}) => {
+  return (
+    <Container>
+      <header>
+        <h1>
+          {title}
+          <img src={icon}/>
+        </h1>
+        <p>{description}</p>
+      </header>
+      <footer>
+        <span>{footerText}</span>
+      </footer>
+    </Container>
+  )
+}
+
+export default MessageBox;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,16 @@
 import React, { useState, useMemo } from  'react';
 
 import ContentHeader from '../../components/ContentHeader';
+import MessageBox from '../../components/MessageBox';
 import SelectInput from '../../components/SelectInput';
 import WalletBox from '../../components/WalletBox';
 
 import expenses from '../../repositories/expenses';
 import gains from '../../repositories/gains';
-import listOfMonths from '../../utils/months'
+import listOfMonths from '../../utils/months';
+
+import happyImg from '../../assets/happy.svg';
+import sadImg from '../../assets/sad.svg';
 
 import {
   Container,
@@ -108,6 +112,12 @@ const Dashboard: React.FC = () => {
           amount={4850.00}
           footerLabel={'atualizado com base nas entradas e saídas'}
           icon='arrowDown'
+        />
+        <MessageBox
+          title='Muito bem!'
+          description='Sua carteira está positiva!'
+          footerText='Continue assim. Considere investir o seu saldo'
+          icon={happyImg}
         />
       </Content>
     </Container>
