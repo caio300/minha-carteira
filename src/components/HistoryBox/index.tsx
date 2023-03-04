567c84f1a4762817308da988c4cc8ca377c738c3
caio300 <caioqs.info@gmail.com>
2023-03-04 20:00:34 -0300

    Formatado os valores da legenda do gráfico de histórico de saldos

--- src/components/HistoryBox/index.tsx
+++ src/components/HistoryBox/index.tsx
@@ -9,6 +9,8 @@ import {
   YAxis,
 } from 'recharts';
 
+import formatCurrency from '../../utils/formatCurrency';
+
 import {
   Container,
   ChartContainer,
@@ -58,7 +60,7 @@ const HistoryBox: React.FC<IHistoryBoxProps> = ({
             <CartesianGrid strokeDasharray='3 3' stroke='#cecece'/>
             <XAxis dataKey='month' stroke='#cecece'/>
             <YAxis />
-            <Tooltip />
+            <Tooltip formatter={(value) => formatCurrency(Number(value))}/>
             <Line
               type='monotone'
               dataKey='amountEntry'
