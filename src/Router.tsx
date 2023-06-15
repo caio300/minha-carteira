09a9963a30eaf15ed2701d2f3e5f2d13bbf1ae40
Caio Queiroz Soares <caioqs.info@gmail.com>
2023-06-15 13:01:57 -0300

    Criando a tela de login

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,9 +4,12 @@ import Layout from "./components/Layout";
 import Dashboard from './pages/Dashboard/index';
 import List from './pages/List/index';
 
+import SignIn from "./pages/SignIn";
+
 export function Router() {
   return (
     <Routes>
+      <Route path="/" element={<SignIn />}/>
       <Route path="/" element={<Layout/>}>
         <Route path="/dashboard" element={<Dashboard />}/>
         <Route path="/list/:type" element={<List/>}/>
