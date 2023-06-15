b56489798fdae224a2106914b7593a79178974a6
Caio Queiroz Soares <caioqs.info@gmail.com>
2023-06-15 12:02:31 -0300

    Trocado otheme pelo botão Toggle

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,7 +1,9 @@
-import React, { useMemo } from  'react';
+import React, { useMemo, useState } from  'react';
 
 import emojis from '../../utils/emojis';
 import Toggle from '../Toggle';
+
+import { useTheme } from '../../hooks/theme';
 
 import {
   Container,
@@ -12,6 +14,15 @@ import {
 
 
 const MainHeader: React.FC = () => {
+  const { toggleTheme, theme } = useTheme();
+
+  const [darkTheme, setDarkTheme] = useState(() => theme.title === 'dark' ? true : false);
+
+  const handleChangeTheme = () => {
+    setDarkTheme(!darkTheme);
+    toggleTheme();
+  }
+
   const emoji = useMemo(() => {
     const indice = Math.floor(Math.random() * emojis.length);
     return emojis[indice]
@@ -20,7 +31,12 @@ const MainHeader: React.FC = () => {
 
   return (
     <Container>
-      <Toggle />
+      <Toggle
+          labelLeft='Light'
+          labelRight='Dark'
+          checked={darkTheme}
+          onChange={handleChangeTheme}
+      />
 
       <Profile>
         <Welcome>Olá, {emoji}</Welcome>
