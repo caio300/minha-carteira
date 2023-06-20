2f2fc1feff6afa77bc1f555bd485bf8db6671c7d
Caio Queiroz Soares <caioqs.info@gmail.com>
2023-06-20 12:37:36 -0300

    Aplicando media queries no menu lateral

--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -9,14 +9,14 @@ export const Container = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 0 10px;
-  border-bottom: 1px solid ${props => props.theme.colors.gray}
+  border-bottom: 1px solid ${props => props.theme.colors.gray};
 
 `;
 
 export const Profile = styled.div`
   display: flex;
   flex-direction: column;
-  color: ${props => props.theme.colors.white}
+  color: ${props => props.theme.colors.white};
 `;
 
 export const Welcome = styled.h3``;
